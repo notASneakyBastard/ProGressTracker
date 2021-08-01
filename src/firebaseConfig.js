da5c5104232ea1614b556e2e3f781fb7d99da1d8
notASneakyBastard <filip.pilot@gmail.com>
2021-08-01 23:31:37 +0200

    Quick env fix

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const config = {
-    apiKey: "AIzaSyD1Pm7aw51T_6l304k6mDgPkHfcFT1e7KQ",
-    authDomain: "progressmonitor-398b2.firebaseapp.com",
-    databaseURL: "https://progressmonitor-398b2.firebaseio.com",
-    projectId: "progressmonitor-398b2",
-    storageBucket: "progressmonitor-398b2.appspot.com",
-    messagingSenderId: "526442779248",
-    appId: "1:526442779248:web:4f0e013790a364f4"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+}
 
-export default config;
+export default config
